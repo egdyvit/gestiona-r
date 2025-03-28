c2c57cedb251d9a146497ee86558cc743c615294
EGDY <ariasegdy@gmail.com>
2025-03-28 11:46:14 -0400

    menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -314,6 +314,7 @@ return [
         [
             'type' => 'sidebar-menu-search',
             'text' => 'search',
+            'icon' => 'nav-icon fas fa-tachometer-alt',
         ],
         [
             'text' => 'blog',
@@ -321,76 +322,55 @@ return [
             'can' => 'manage-blog',
         ],
         [
-            'text' => 'pages',
-            'url' => 'admin/pages',
-            'icon' => 'far fa-fw fa-file',
-            'label' => 4,
-            'label_color' => 'success',
+            'text' => 'Inicio',
+            'route' => 'home',
+            'icon' => 'nav-icon fas fa-tachometer-alt',
         ],
-        ['header' => 'account_settings'],
         [
-            'text' => 'profile',
+            'text' => 'Crear Proyecto',
+            'route' => 'home',
+            'icon' => 'nav-icon fas fa-edit',
+        ],
+        [
+            'text' => 'Carga de Avances',
+            'route' => 'home',
+            'icon' => 'nav-icon far fa-plus-square',
+        ],
+        [
+            'text' => 'Lista de Proyectos',
+            'route' => 'home',
+            'icon' => 'nav-icon fas fa-chart-pie',
+        ],
+        [
+            'text' => 'Generar Reporte',
+            'route' => 'home',
+            'icon' => 'nav-icon fas fa-book',
+        ],
+        ['header' => 'Gestion de Personal'],
+        [
+            'text' => 'Nuevo Personal',
             'url' => 'admin/settings',
             'icon' => 'fas fa-fw fa-user',
         ],
         [
-            'text' => 'change_password',
+            'text' => 'Lista de Personal',
             'url' => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'icon' => 'fas fa-fw fa-file',
         ],
+        ['header' => 'Gestion de Cuenta'],
         [
-            'text' => 'multilevel',
-            'icon' => 'fas fa-fw fa-share',
+            'text' => 'Cuenta',
+            'icon' => 'nav-icon fas fa-ellipsis-h',
             'submenu' => [
                 [
-                    'text' => 'level_one',
+                    'text' => 'Perfil',
                     'url' => '#',
                 ],
                 [
-                    'text' => 'level_one',
-                    'url' => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                        ],
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
+                    'text' => 'Contraseña',
                     'url' => '#',
                 ],
             ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text' => 'important',
-            'icon_color' => 'red',
-            'url' => '#',
-        ],
-        [
-            'text' => 'warning',
-            'icon_color' => 'yellow',
-            'url' => '#',
-        ],
-        [
-            'text' => 'information',
-            'icon_color' => 'cyan',
-            'url' => '#',
         ],
     ],
 
